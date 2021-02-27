bebf6877cf660134a25140f96cbc0fbc0fe3b306
kang king <whtSir@outlook.com>
2021-02-27 17:27:40 +0800

    重构

--- README.md
+++ README.md
@@ -1,2 +1,199 @@
 # redux-kangking
- 简化redux开发流程
+
+***You don't need to write a reducer!***
+
+***you don't need to pay attention to Immutable!***
+
+***You don't even need to write an action!***
+
+***Just focus on state transition.***
+
+## Conception
+
+1. state processor
+   - handle slice state change
+2. state manager
+   - hold all processors
+   - initialize the execution chain
+   - re-establish execution chain at the right time (coming soon)
+
+## Installation
+
+```bash
+npm install immer redux-kangking
+```
+
+## Example
+
+**defining state types (non-essential)**
+
+```ts
+export default interface AppState {
+   count?: number;
+   tasks?: Array<Task>
+}
+
+export interface Task {
+   id: string;
+   title: string;
+   isCompleted: boolean;
+   createdTime?: Date;
+   updatedTime?: Date;
+}
+```
+
+**create redux store**
+
+```ts
+import {applyMiddleware, createStore} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {appStateReducer} from 'redux-kangking';
+
+/**
+ * import {createReducer, ReduxStateProcessor} from 'redux-kangking';
+ *
+ * // you can create other reducers like this:
+ * const otherStateManager = new ReduxStateProcessor.AppStateManager<AppState>();
+ * const otherStateReducer = createReducer(appStateManager);
+ */
+
+// 'appStateReducer' is a reducer created by 'redux-kangking' by default
+export default createStore(appStateReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+```
+
+**create processor**
+
+```ts
+import {Dispatch} from 'redux';
+import {appStateManager, ReduxStateProcessor} from 'redux-kangking';
+import AppState from '../../redux/state';
+
+/**
+ * IncrementProcessor 和 DecrementProcessor 共用一块状态(shared slice state), 所以这里提取一个父类
+ * 如果有其它 'increment' 操作也可以继承 IncrementProcessor 实现逻辑复用, 如: IncrementAsyncProcessor
+ * 如果搭配 redux-observable 或 redux-saga 甚至不需要修改 getActionCreator
+ */
+abstract class CounterProcessor extends ReduxStateProcessor<AppState, number> {
+   getSliceState(appState: AppState): number {
+      return appState.count || 0;
+   }
+}
+
+/**
+ * handle increment action
+ */
+class IncrementProcessor extends CounterProcessor {
+   getActionName() {
+      // identifiable
+      return 'increment';
+   }
+
+   // update state
+   protected handleState(appState: AppState, prevState: number, actionData: number): void {
+      appState.count = prevState + actionData;
+   }
+}
+
+/**
+ * handle decrement action
+ */
+class DecrementProcessor extends CounterProcessor {
+   protected handleState(appState: AppState, prevState: number, actionData: number): void {
+      appState.count = prevState - actionData;
+   }
+}
+
+/**
+ * with redux-thunk
+ */
+export class IncrementAsyncProcessor extends IncrementProcessor {
+   getActionCreator(): any {
+      return (data: number) => {
+         return (dispatch: Dispatch) => {
+            setTimeout(() => dispatch(super.getActionCreator()(data)), 1000);
+         };
+      };
+   }
+}
+
+export const incrementProcessor = new IncrementProcessor();
+export const decrementProcessor = new DecrementProcessor();
+export const incrementAsyncProcessor = new IncrementAsyncProcessor();
+// necessary steps 
+appStateManager.addProcessor(incrementProcessor, decrementProcessor, incrementAsyncProcessor);
+```
+
+**use processor**
+
+```typescript jsx
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {CreateActionFunc} from 'redux-kangking';
+import AppState from '../../redux/state';
+import {decrementProcessor, incrementAsyncProcessor, incrementProcessor} from './counter.state';
+
+interface CounterPropTypes {
+   count: number;
+   doIncrement: CreateActionFunc<number>,
+   doDecrement: CreateActionFunc<number>,
+   doIncrementAsync: CreateActionFunc<number>
+}
+
+class Counter extends Component<CounterPropTypes> {
+
+   numberElement = React.createRef<HTMLSelectElement>();
+
+   private increment = () => {
+      const numberSelected = this.numberElement.current!.value;
+      this.props.doIncrement(parseInt(numberSelected));
+   };
+
+   private decrement = () => {
+      const numberSelected = this.numberElement.current!.value;
+      this.props.doDecrement(parseInt(numberSelected));
+   };
+
+   private incrementAsync = () => {
+      const numberSelected = this.numberElement.current!.value;
+      this.props.doIncrementAsync(parseInt(numberSelected));
+   };
+
+   render() {
+      return (
+         <div className="row">
+            <div className="col-12">
+               <h3>当前结果: {this.props.count}</h3>
+            </div>
+            <div className="col-12 d-flex justify-content-start">
+               <select ref={this.numberElement} className="mr-2" style={{width: '150px'}}>
+                  <option>1</option>
+                  <option>2</option>
+               </select>
+               <button className="btn btn-outline-primary mr-2" onClick={this.increment}>加</button>
+               <button className="btn btn-outline-primary mr-2" onClick={this.decrement}>减</button>
+               <button className="btn btn-outline-primary mr-2" onClick={this.incrementAsync}>异步加</button>
+            </div>
+         </div>
+      );
+   }
+}
+
+// with react-redux
+export default connect(
+   (state: AppState) => ({count: incrementProcessor.getSliceState(state)}),
+   {
+      doIncrement: incrementProcessor.getActionCreator(),
+      doDecrement: decrementProcessor.getActionCreator(),
+      doIncrementAsync: incrementAsyncProcessor.getActionCreator()
+   }
+)(Counter);
+```
+
+[Complete Example](https://github.com/x-wk/redux-kangking)
+
+***Less is More, haha😊***
+
+## License
+
+[MIT](https://github.com/x-wk/redux-kangking/blob/main/LICENSE.md)
--- src/processor.ts
+++ src/processor.ts
@@ -10,7 +10,7 @@ function padLeft(num: number, size = 5): string {
    return _str.slice(_str.length - size);
 }
 
-interface CreateActionFunc<T> {
+export interface CreateActionFunc<T> {
    (actionData: T): AnyAction;
 }
 
@@ -26,39 +26,37 @@ interface ReduxActionCreator<T> {
  * D: slice state shape
  * T: action data type
  */
-abstract class ReduxStateHandler<S, D, T> {
+abstract class ReduxStateHandler<S, D, T = any> {
 
    /**
-    * 从应用状态中获取指定 "部分" 状态(注: )
-    *    1. 勿返回默认值, 否则请根据情况重写 initialized 方法
-    *    2. 若返回多个 "部分" 的状态, 其中一些可能已初始化, 请根据情况重写 initialized 方法
+    * 从应用状态中获取指定的 "部分状态", 如果未初始化可以返回默认状态
     * @param appState 应用状态
     */
-   abstract getSliceState(appState: S): D | undefined;
+   abstract getSliceState(appState: S): D;
 
    /**
     * 检查初始化状态, 如果返回: false , 后续会执行 initSliceState 方法
     * @param appState 应用状态
     * @protected
     */
-   protected initialized(appState: S): boolean {
-      return !!this.getSliceState(appState);
-   }
+   // protected initialized(appState: S): boolean {return !!this.getSliceState(appState);}
 
    /**
     * 设置初始状态
     * @param appState
     */
-   protected abstract initSliceState(appState: S): void;
+
+   // protected abstract initSliceState(appState: S): void;
 
    /**
     * 处理状态
     * 直接修改appState, 无需担心会改变原状态. 如: appState.count = 0
-    * @param appState 应用状态的代理对象(注: 传递整个应用状态是否存在隐患还有待论证; 只传部分状态也非常简单)
+    * @param appState 可直接修改. 应用状态的副本集, 需要获取其它 "部分状态" 时很有用
+    * @param prevState 可直接修改. 本次操作执行时的状态
     * @param actionData 操作数据
     * @protected
     */
-   protected abstract handleState(appState: S, actionData: T): void;
+   protected abstract handleState(appState: S, prevState: D, actionData: T): void;
 
 }
 
@@ -75,11 +73,7 @@ interface ReduxStateProcessorManager<S> {
 }
 
 /**
- * 更多特性有待完善:
- *    1. 按需加载
- *    2. 状态快照
- *    3. 按调用频次动态排序
- *    4. ...
+ * 更多特性有待完善
  */
 export abstract class ReduxStateProcessor<S, D, T = D> extends ReduxStateHandler<S, D, T> implements ReduxActionCreator<T> {
 
@@ -94,9 +88,6 @@ export abstract class ReduxStateProcessor<S, D, T = D> extends ReduxStateHandler
       this._actionName = this.getActionName() || `@@ACT#${padLeft(this._initOrder)}`;
    }
 
-   /**
-    * @param {any} processor
-    */
    private set next(processor: ReduxStateProcessor<S, any>) {
       this._next = processor;
    }
@@ -109,12 +100,16 @@ export abstract class ReduxStateProcessor<S, D, T = D> extends ReduxStateHandler
       return this._actionName;
    }
 
-   // 子类可覆盖
+   /**
+    * 子类可覆盖, 如搭配 redux-thunk 时返回一个函数型 Action
+    */
    getActionCreator() {
       return (actionData: T) => ({type: this.getActionName(), actionData});
    }
 
-   // 子类可覆盖
+   /**
+    * 子类可覆盖, 实现更复杂的逻辑判断
+    */
    protected isSupport(actionName: string): boolean {
       return this.getActionName() === actionName;
    }
@@ -122,34 +117,36 @@ export abstract class ReduxStateProcessor<S, D, T = D> extends ReduxStateHandler
    process(appState: S, action: AnyAction): S {
 
       // 执行初始化
-      const initializedState = produce(appState, (draftState: S) => {
-         if (this.initialized(draftState)) {
-            return false;
-         }
-         this.initSliceState(draftState);
-      }) || appState;
+      /*
+            const initializedState = produce(appState, (draftState: S) => {
+               if (this.initialized(draftState)) {
+                  return false;
+               }
+               this.initSliceState(draftState);
+            }) || appState;
+      */
 
       // 执行状态变更
       const {type, actionData} = action;
       if (this.isSupport(type)) {
-         return produce(initializedState, (draftState: S) => {
-            this.handleState(draftState, actionData);
+         return produce(appState, (draftState: S) => {
+            this.handleState(draftState, this.getSliceState(draftState), actionData);
          });
       }
 
       // 传递给下一个处理器
       if (this._next) {
-         return this._next.process(initializedState, action);
+         return this._next.process(appState, action);
       }
 
-      // AppStateManager兜底, 否则提示错误
+      // AppStateManager必须兜底
       throw new Error('未配置应用状态处理器[AppStateProcessor]');
    }
 
    /**
-    * APP状态管理器
+    * APP状态管理器, 可以实例多个 Manager 实现处理器分组
     */
-   static AppStateManager = class AppStateManager<S> extends ReduxStateProcessor<S, any> implements ReduxStateProcessorManager<S> {
+   static AppStateManager = class AppStateManager<S> extends ReduxStateProcessor<S, S> implements ReduxStateProcessorManager<S> {
       private _first: ReduxStateProcessor<S, any> = this;
       private _processors: Array<ReduxStateProcessor<S, any>> = [];
 
@@ -176,19 +173,11 @@ export abstract class ReduxStateProcessor<S, D, T = D> extends ReduxStateHandler
          return this._first;
       }
 
-      protected initialized(appState: S): boolean {
-         return true;
-      }
-
-      protected initSliceState(appState: S): void {
+      protected handleState(appState: S, prevState: any, actionData: any): void {
          // do nothing
       }
 
-      protected handleState(appState: S, actionData: any): void {
-         // do nothing
-      }
-
-      getSliceState(appState: S): any | undefined {
+      getSliceState(appState: S): any {
          return appState;
       }
 
@@ -199,16 +188,14 @@ export abstract class ReduxStateProcessor<S, D, T = D> extends ReduxStateHandler
 }
 
 // 用于创建reducer
-export function reducerCreator<S>(stateManager: ReduxStateProcessorManager<S>): Reducer<S> {
+export function createReducer<S>(stateManager: ReduxStateProcessorManager<S>): Reducer<S> {
    return (appState: S = {} as any, action: AnyAction) => {
       return stateManager.getProcessor().process(appState, action);
    };
 }
 
 // default app stateManager
-const appStateManager = new ReduxStateProcessor.AppStateManager<any>();
+export const appStateManager = new ReduxStateProcessor.AppStateManager<any>();
 
 // default app stateReducer
-export const appStateReducer = reducerCreator(appStateManager);
-
-export default appStateManager;
+export const appStateReducer = createReducer(appStateManager);
